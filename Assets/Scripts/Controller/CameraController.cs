f75625e66ae43ee7f2987c4f881bbe71dbe25db2
harunkll <harunkll46@gmail.com>
2020-10-06 23:05:34 +0300

    06-10-2020-23-05

--- Assets/Scripts/Controller/CameraController.cs
+++ Assets/Scripts/Controller/CameraController.cs
@@ -13,9 +13,10 @@ public class CameraController : MonoBehaviour
         CinemachineCore.GetInputAxis = GetInputAxis;
     }
     private void Update()
-    {if(Input.GetAxis(""))
+    {
+        //if(Input.GetAxis(""))
         _freeLookActive = Input.GetMouseButton(1); // 0 = left mouse btn or 1 = right
-        Input.GetAxis(Jo);
+        //Input.GetAxis(Jo);
     }
     private void LateUpdate()
     {
@@ -26,21 +27,21 @@ public class CameraController : MonoBehaviour
         
     }
     private void Awake() {
-        //controls = new PlayerControls();
+        controls = new PlayerControls();
         //controls.Gameplay.RStick.performed += ctx => move =ctx.ReadValue<Vector2>();
        // controls.Gameplay.RStick.canceled += ctx => move = Vector2.zero;
     }
 
-    void RStick() {
-        Debug.Log("Rstick");
-    }
-    private void OnEnable() {
-        controls.Gameplay.Enable();
-        Debug.Log("Enable");
-    }
-    private void OnDisable() {
-        controls.Gameplay.Disable();
-    }
+    //void RStick() {
+    //    Debug.Log("Rstick");
+    //}
+    //private void OnEnable() {
+    //    controls.Gameplay.Enable();
+    //    Debug.Log("Enable");
+    //}
+    //private void OnDisable() {
+    //    controls.Gameplay.Disable();
+    //}
 
 
 }
